b8705186c45a0b4bdc4a8c1ce397463d42faf021
tarun10051990 <kaushik.tarun90@gmail.com>
2024-08-20 12:00:23 +0530

    second commit

--- src/main/java/com/taskmanagement/demo/TaskmanagementApplication.java
+++ src/main/java/com/taskmanagement/demo/TaskmanagementApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TaskmanagementApplication {
 
 	public static void main(String[] args) {
+
+		System.out.println("testing");
 		SpringApplication.run(TaskmanagementApplication.class, args);
 	}
 
